959dc8a5f317cae524d60c1f961dcf3cbd87bb77
chasinghope <bobjiang1045@163.com>
2024-09-25 17:06:49 +0800

    AutoRadio

---
+++ Runtime/Components/AutoRadio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Congroo.Core
+{
+    public class AutoRadio : MonoBehaviour
+    {
+        public Vector2 Radio;
+        private void Start()
+        {
+            int targetWidth = Mathf.CeilToInt(Screen.currentResolution.height * Radio.x / Radio.y);
+            Screen.SetResolution(targetWidth, Screen.currentResolution.height, false);
+            Debug.Log("[AutoRadio] Work...");
+        }
+    }
+}
